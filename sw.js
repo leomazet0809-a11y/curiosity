be656d99406e306847c7480869bf1a82afc7efe2
leomazet0809-a11y <leomazet0809@gmail.com>
2026-06-06 15:27:00 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'curiosity-v1';
+const FILES = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        return caches.open(CACHE).then(function(c) {
+          c.put(e.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(function() {
+      return caches.match('/index.html');
+    })
+  );
+});
